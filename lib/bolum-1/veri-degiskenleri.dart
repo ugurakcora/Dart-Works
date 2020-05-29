bd9a74fb511c3cb1386dc9aac13bfc2ed9285120
Uğur <uakcora97@gmail.com>
2020-05-29 15:08:54 +0300

    second commit

---
+++ lib/bolum-1/veri-degiskenleri.dart
@@ -0,0 +1,13 @@
+main(List<String> args) {
+  // int
+  int a = 5;
+  int b = 3;
+  int c = a + b;
+  print(c);
+
+  // double
+  double d = 2.3;
+  double e = 3.5;
+  double f = d + e;
+  print(f);
+}
