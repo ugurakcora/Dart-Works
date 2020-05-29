d79d3109a59aa9485860085949edfd22d665c9a6
Uğur <uakcora97@gmail.com>
2020-05-29 19:50:36 +0300

    ternary operation

--- lib/bolum-2/if-else-elseif.dart
+++ lib/bolum-2/if-else-elseif.dart
@@ -1,5 +1,4 @@
 main(List<String> args) {
-
   // if else elseif kullanımı
   int a = 5;
   int b = 3;
@@ -23,33 +22,58 @@ main(List<String> args) {
     print("$e $f'den büyük");
   } else if (e < f) {
     print("$f $e'den büyük");
-  }
-  else{
+  } else {
     print("eşit");
   }
 
-
   // nor ortalama
-  int ortalama=75;
-  if (ortalama<=40) {
+  int ortalama = 75;
+  if (ortalama <= 40) {
     print("FF");
-  }
-  else if (ortalama <= 50){
+  } else if (ortalama <= 50) {
     print("DD");
-  }
-  else if (ortalama <= 60){
+  } else if (ortalama <= 60) {
     print("CC");
-  }
-   else if (ortalama <= 70){
+  } else if (ortalama <= 70) {
     print("BC");
-  }
-  else if (ortalama <= 80){
+  } else if (ortalama <= 80) {
     print("BB");
-  }
-   else if (ortalama <= 90){
+  } else if (ortalama <= 90) {
     print("AB");
-  }
-  else{
+  } else {
     print("AA");
   }
+
+  // ternary operatörü (Kısa if kullanımı)
+
+  int s1 = 5;
+  var s2 = 4;
+  int kucukSayi;
+
+  // if else hali
+  if (s1 < s2) {
+    kucukSayi = s1;
+  } else {
+    kucukSayi = s2;
+  }
+  print(kucukSayi);
+
+  // ternary hali
+  // ? = if hali
+  // : = else hali
+  s1 < s2 ? kucukSayi = s1 : kucukSayi = s2;
+  print("Küçük sayı : $kucukSayi");
+
+  // bu da başka bir kullanım yöntemi
+  kucukSayi = s1 < s2 ? s1 : s2;
+  print("Küçük sayı : $kucukSayi");
+
+  String isim;
+  String soyisim ="Akçora";
+  String mesaj;
+
+
+  // ?? = null değil mi diye bakıyor eğer nullsa ekrana yazdırmıyor.
+  mesaj = isim ?? soyisim;
+  print("merhaba $mesaj");
 }
