f3ad99819c5c44e12ee4f3df70f81f34676fab51
Uğur <uakcora97@gmail.com>
2020-06-11 01:25:20 +0300

    buyuyenList

---
+++ lib/bolum-4/dinamik-list.dart
@@ -0,0 +1,12 @@
+/**
+ * Dinamik olarak büyüyen küçülen koleksiyonlarımız varsa sabit uzunluklu listeler yerine
+ * buyuyen listeleri kullanırız. Bu listelere varsayılan olarak boyut vermemiz gerekmez.
+ * 
+ * Tanımlanması sabit uzunluklu dizilere benzer sadece burada listenin boyutunu belirtmeyiz.
+ * add eleman ekler, 
+ * clear tüm elemanları siler, 
+ * remove verilen elemanı siler,
+ * removeAt belirtilen indextekielemanı siler
+ * Ayrıca [] kullanarak belli bir indexteki elemanı güncelleyebilirz.
+ */
+main(List<String> args) {}
